b007b4f002d31589250c479f2c5d5a28cd573437
khlick <khrisgriffis@ucla.edu>
2018-03-15 11:08:16 -0700

    last lec

--- lib/_notebooks/Tools/stats.py
+++ lib/_notebooks/Tools/stats.py
@@ -149,6 +149,8 @@ def getQuants(data,returnPercentiles = False):
         return searchQuants(data)
     #otherwise I want the function so i can use it
     return searchQuants;
+
+
 '''
 expectTable
 Get the expected contingency table.
@@ -165,6 +167,7 @@ def expectTable(observeTable,axis=0):
         probs = colSums / np.sum(colSums);
         prod = rowSums;
     return pd.DataFrame(np.outer(prod,probs), index=observeTable.index, columns=observeTable.columns);
+
 '''
 chi2
 compute chi squared from a contingency table
@@ -174,3 +177,97 @@ def chi2(observed, **expectArgs):
     expected = np.ravel(expectTable(observed, **expectArgs));
     observed = np.ravel(observed);
     return np.sum((observed-expected)**2/expected);
+
+
+'''
+Risk function, just for helping make it readable
+'''
+def getRisk(contingencyMatrix):
+    # outcomes need to be on rows
+    rowSums= np.sum(contingencyMatrix, axis=0);
+    # now, divide each row by the sum of the row
+    #I guess, make a matrix the same size as input matrix
+    sumMat = np.repeat(rowSums,contingencyMatrix.shape[1]).reshape((-1,contingencyMatrix.shape[1])).T;
+    return contingencyMatrix / sumMat
+
+'''
+Relative Risk
+RR = riskA/riskB
+'''
+def getRelativeRisk(conTable, riskVar, colInterest):
+    '''
+    Requires: 
+      conTable is a pandas DF with 2 columns and named indeces
+      outcomes are in the rows
+      riskVar is the name of the row index
+      colInterest is number or name of column
+    '''
+    if isinstance(colInterest, str):
+        colNumber = conTable.columns.get_loc(colInterest);
+    else:
+        colNumber = colInterest;
+        colInterest = conTable.columns[colNumber];
+    #get risks
+    riskObserved = pd.DataFrame(
+        getRisk(conTable.as_matrix()),
+        index=conTable.index, 
+        columns=conTable.columns
+    );
+    riskOfInterest = riskObserved.loc[riskVar];
+    if not colNumber:
+        otherCol = 1;
+    else:
+        otherCol = 0;
+    RR = riskOfInterest[colNumber]/riskOfInterest[otherCol];
+    return RR
+
+
+'''
+Chi Squared bootstrap
+'''
+def chi2boot(observed, margin=0, B=100):
+    '''
+    Computes chi-squared and bootstrap NHST
+    margin input indicates which margin of the observed contingency table
+    contains the outcome varaible where 0=index (rows) and 1=columns
+    '''
+    x2_observed = chi2(observed);
+    #detect margin and force outcome in 1st column
+    if not margin:
+        outcome_ = observed.index.tolist();
+        test_ = observed.columns.tolist();
+    else:
+        outcome_ = observed.columns.tolist();
+        test_ = observed.index.tolist();
+    observedLong = pd.DataFrame(
+    utils.expandGrid(
+        outcome_,
+        test_, 
+        doPaste=False, 
+        expansion = np.ravel(np.int64(observed), order='F')
+    ), 
+    columns=['Outcome', 'Test']
+    );
+    #boot
+    chi2_NHST = np.zeros(B);
+    # make a copy of the data to run the nhst randomization
+    nhstData = observedLong.copy();
+    nRows = len(observedLong.index);
+    for b in range(B):
+        # NHST randomization
+        nullOutcome = observedLong['Outcome'].sample(nRows,replace=True);
+        nhstData['Outcome'] = nullOutcome.values;
+        NHSTTable = pd.crosstab(
+          nhstData['Outcome'],
+          nhstData['Test']
+        );
+        #Pandas reorganizes stuff, let's make sure we get it back in order
+        NHSTTable = NHSTTable.loc[outcome_,test_];
+        chi2_NHST[b] = chi2(NHSTTable)
+    return {
+    'Bootstraps': chi2_NHST,
+    'Observed': x2_observed,
+    'pvalue': np.mean(chi2_NHST >= x2_observed)
+    }
+
+
